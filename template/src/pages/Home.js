ad4c7384a1812fad4d5f04acb4ac22d91fe73b29
folland87 <frederic@olland.me>
2021-01-07 10:51:43 +0100

    fix quote

--- template/src/pages/Home.js
+++ template/src/pages/Home.js
@@ -1,4 +1,4 @@
-import { Button } from "@folland87/puzzle";
+import { Button } from '@folland87/puzzle';
 
 export default function Home() {
   return <Button color="primary" m={6}>Hello world</Button>;
