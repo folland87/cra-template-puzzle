78542070214249907f0ac8dcdb40039ac1e3924c
folland87 <frederic@olland.me>
2021-01-07 10:44:16 +0100

    fix quotes

--- template/src/pages/Home.js
+++ template/src/pages/Home.js
@@ -1,17 +1,5 @@
 import { Button } from "@folland87/puzzle";
 
 export default function Home() {
-  const test = [
-    "afkfkhghk",
-    "bvfglkvk",
-    "cvghkjvglj",
-    "dcvghkcghlkcghkcghkcglhcgkhbckhggv;,",
-    "ecghk",
-    "fcghkch",
-  ];
-  return (
-    <Button color="primary" m={6}>
-      Hello world
-    </Button>
-  );
+  return <Button color="primary" m={6}>Hello world</Button>;
 }
